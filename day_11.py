41fd1d5f6c3ea1169539a16402610cfd88eb6226
ChaduaryFaizan <ashfaqfaizan18@gmail.com>
2026-09-14 06:13:07 +0500

    Update day_11.py

--- day_11.py
+++ day_11.py
@@ -1,14 +1,17 @@
+from random import randint
 from random import choice
 """Black jack game 
 A Man Against the Possibility"""
-cards=(2,3,4,5,6,7,8,9,10,10,10,10,11)
+
+cards=[2,3,4,5,6,7,8,9,10,10,10,10,11]
 
 
 continue_or_cheak=input("Do You want to play Game . 'y' for continuing or 'n' for exit : ").lower()
-users_cards=()
+users_cards=[]
+computer_cards=[]
 if continue_or_cheak=='y':
     
-    users_cards+=(choice(cards))
+    users_cards.append(choice(cards))
     print(users_cards)
 
 
