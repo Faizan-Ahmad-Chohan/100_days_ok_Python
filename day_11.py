da797730a51dff65da38b451e86871a750100f57
ChaduaryFaizan <ashfaqfaizan18@gmail.com>
2026-09-19 01:59:56 +0500

    Update day_11.py

--- day_11.py
+++ day_11.py
@@ -13,12 +13,14 @@ if permition=='y':
 
     while True:     
         print(f"Your cards are: {users_cards}")
-        print(f"Computer's first card is : {computer_cards[0]}")  
+        print(f"Computer's first card is : {computer_cards}")  
         more_card=input("If you want to draw more cards \" yes 'y',no 'n'\"") 
         if more_card=='y':
             users_cards.append(draw_card()) 
 
-            
+        if calculate_score(computer_cards)<16:
+             computer_cards.append(draw_card())
+               
             
  
 
