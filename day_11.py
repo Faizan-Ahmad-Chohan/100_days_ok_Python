ca8dc57ec0b8222f138b277635a0c94094a15fa7
ChaduaryFaizan <ashfaqfaizan18@gmail.com>
2026-09-19 02:04:19 +0500

    Update day_11.py

--- day_11.py
+++ day_11.py
@@ -2,7 +2,7 @@ from random import randint
 from random import choice 
 from functions import draw_card,calculate_score
 
-permition=input("If you want to play game  type 'y'  or type 'n' to exit : ")
+permition=input("If you want to play game  type 'y'  or type 'n' to exit : ").lower()
 if permition=='y':
     users_cards=[]
     computer_cards=[]
@@ -14,13 +14,13 @@ if permition=='y':
     while True:     
         print(f"Your cards are: {users_cards}")
         print(f"Computer's first card is : {computer_cards[0]}")  
-        more_card=input("If you want to draw more cards \" yes 'y',no 'n'\"") 
-        if more_card=='y':
+        more_card=input("If you want to draw more cards \" yes 'y',no 'n'\"").lower()
+        if more_card=='y': 
             users_cards.append(draw_card()) 
 
         if calculate_score(computer_cards)<16:
             computer_cards.append(draw_card())
-               
+              
             
  
 
