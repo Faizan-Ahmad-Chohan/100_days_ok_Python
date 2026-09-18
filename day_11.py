6877ace713363249e4a129b4a00223389e102650
ChaduaryFaizan <ashfaqfaizan18@gmail.com>
2026-09-19 01:41:09 +0500

    Update day_11.py

--- day_11.py
+++ day_11.py
@@ -9,9 +9,12 @@ if permition=='y':
     for i in range(2):
              users_cards.append(draw_card())
              computer_cards.append(draw_card())
+             print(f"Your cards are: {users_cards}")
+             print(f"Computer's first card is : {computer_cards[0]}")  
+    while True:      
+         
+        more_card=input("If you want to draw more cards \" yes 'y',no 'n'\"")  
             
-    print(f"Your cards are: {users_cards}")
-    print(f"Computer's first card is : {computer_cards[0]}")         
             
  
 
