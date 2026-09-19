a496cd7ddd67a548f74b1f5cd5e46a28ee7ab12b
ChaduaryFaizan <ashfaqfaizan18@gmail.com>
2026-09-19 13:00:28 +0500

    Update day_11.py

--- day_11.py
+++ day_11.py
@@ -9,21 +9,15 @@ if permition=='y':
     for i in range(2):
             users_cards.append(draw_card())
             computer_cards.append(draw_card())
-          
-
-    while True:     
-        print(f"Your cards are: {users_cards}")
-        print(f"Computer's first card is : {computer_cards[0]}")  
-        more_card=input("If you want to draw more cards \" yes 'y',no 'n'\"").lower()
-        if more_card=='y': 
-            users_cards.append(draw_card()) 
-        
-        computers_total=calculate_score(computer_cards)
-        while  computers_total <=16:
+    while True  :   
+        print(f"Your cards are : {users_cards}")
+        print(f"Computer's first card is : {computer_cards}")
+        more_card=input("Do you want to draw more cards 'y' ,'n': ")
+        if more_card=='y':
+            users_cards.append(draw_card())
+        if calculate_score(computer_cards)<16:
             computer_cards.append(draw_card())
-            if ace_case(computer_cards):
-                computers_total=ace_case(computer_cards)
-        print(computers_total)
+
        
              
               
