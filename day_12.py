d45ddde8e6e886dd942eaacdf9ee863c17d06033
ChaduaryFaizan <ashfaqfaizan18@gmail.com>
2026-09-21 16:20:18 +0500

    planing functions
     first function for choosing difficulties  in number guesser.

--- day_12.py
+++ day_12.py
@@ -1,4 +1,21 @@
-def difficulty_selection(type):
+def difficulty_selection():
+    type=input("Chose a difficulty , 'easy' ,'difficult','hard'").lower()
+    loop_exit=True
     no_of_attempts=0
-    if type=='hard':
-        no_of_attempts=5
+    while loop_exit:
+        if type=='hard':
+            no_of_attempts=5
+            loop_exit=False
+            return no_of_attempts
+        elif type=='difficult':
+                no_of_attempts=8
+                loop_exit=False
+                return no_of_attempts
+        elif type=="easy":
+            no_of_attempts=10
+            loop_exit=False
+            return no_of_attempts
+        else:
+            print("Choose  avRelevant Option ")
+            type=input("Chose a difficulty , 'easy' ,'difficult','hard'").lower()
+
