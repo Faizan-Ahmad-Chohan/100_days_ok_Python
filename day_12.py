59ee7938fff1fa615a881549872667bd39b3dc49
ChaduaryFaizan <ashfaqfaizan18@gmail.com>
2026-09-21 16:56:59 +0500

    Update day_12.py

--- day_12.py
+++ day_12.py
@@ -1,5 +1,6 @@
+import random
 def difficulty_selection():
-    type=input("Chose a difficulty , 'easy' ,'difficult','hard'").lower()
+    type=input("Chose a difficulty , 'easy' ,'difficult','hard' : ").lower()
     loop_exit=True
     no_of_attempts=0
     while loop_exit:
@@ -18,4 +19,30 @@ def difficulty_selection():
         else:
             print("Choose  avRelevant Option ")
             type=input("Chose a difficulty , 'easy' ,'difficult','hard'").lower()
+
+def number_gusser(no_of_attempts):
+    number=random.randint(1,100)
+    print(number)
+    while no_of_attempts>0:
+        print(f" You have {no_of_attempts} attempts left to guess the number. ")
+        guess=int(input("Guess a number : "))
+        if guess==number:
+            print(" You Nailed it \n Whoo You made it !!!!")
+            break
+
+        elif guess>number:
+                print(f" Too high\n Guess again ")
+                no_of_attempts -= 1
+
+        elif guess<number:
+            print(f" Too low\n Guess again ")
+            no_of_attempts -= 1
+
+
+
+
+
+
+
+
 
