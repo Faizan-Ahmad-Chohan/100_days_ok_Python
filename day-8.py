566aac62b7097fecd0d65504e7e33bc28c1d4b64
ChaduaryFaizan <ashfaqfaizan18@gmail.com>
2026-09-23 13:43:53 +0500

    Update day-8.py

--- day-8.py
+++ day-8.py
@@ -1,6 +1,30 @@
 from alphabet import alphabets# cieser cifer
-from enc_dec_function import ceasercyfer
 from logo import logo_ceaser_cypher
+
+def ceasercyfer(text,shift,encode_or_decode):
+
+
+    
+    enc_or_dec_mesg=""
+    if encode_or_decode == "decode":
+        shift  *=-1
+    elif encode_or_decode=="encode":
+        shift *= 1
+    else :
+        print("     Please select a valid option    ")
+    for letter in text:
+       
+        index_total=0
+        if letter in alphabets:
+            index_total=alphabets.index(letter) + shift 
+            total_shift = index_total % len(alphabets)
+            enc_or_dec_mesg += alphabets[total_shift]
+        elif letter not in alphabets:
+            enc_or_dec_mesg+=letter
+    print(f"The {encode_or_decode}d reasult is : {enc_or_dec_mesg}")      
+        
+
+
 print(logo_ceaser_cypher)
 first_attempt=True
 while first_attempt:
